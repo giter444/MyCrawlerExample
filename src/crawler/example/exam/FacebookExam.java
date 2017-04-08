ceb47cd074b0f9a8c036a542536504c9017b4098
giter444 <stu86299@gmail.com>
2017-04-08 16:44:06 +0800

    取得任一篇或多篇文章的 Reactions 總數 同下

--- src/crawler/example/exam/FacebookExam.java
+++ src/crawler/example/exam/FacebookExam.java
@@ -23,10 +23,10 @@ public class FacebookExam {
 		
 		// 遠端資料路徑
 
-		String uri = 
+		String uri =
 				"https://graph.facebook.com/v2.6"
-				+ "/search?q="
-				+ "&access_token=";
+				+ "/search?q=%E9%9D%A0%E5%8C%97&type=page&limit=1000&fields=id,reactions.limit(0).summary(total_count)"
+				+ "&access_token=EAACEdEose0cBAMzLyMlMxZAY73J1TBiZAHJhrF8du8fLhMmGYrUA3W9ezJDQk9oZAa07o4F2drZAL2k1cCtgdpl5LZClZCo6733A3xddNbjSAoTxEZBWY4rKnzLWTU3I0KMgT0qsw6eIRfdzg9RZCIkuTZCtGZCyzct657fywQpsFCNAZDZD";
 
 
 		Elements elems =
@@ -39,9 +39,8 @@ public class FacebookExam {
 		// 遂筆處理
 		for( Element data: elems ){
 			String id = data.select("id").text();
-
 			// FIXIT
-			String reactions = "";
+			String reactions = data.select("reactions.limit(0).summary(total_count)").text();
 
 
 			output += id + "," + reactions + "\n";
